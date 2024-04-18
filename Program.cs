eafb26af483877f0ffe48f8a1d3b2b9cff4c1e36
Heidy <heidyguevara23@hotmail.com>
2024-04-18 10:11:56 +0200

    Configuration of Newtonsoft.Json with save and load data. Dsiplay of Main Menu

--- Program.cs
+++ Program.cs
@@ -1,4 +1,67 @@
 ﻿namespace Wild_Project1
 {
-   
+    using System;
+    using System.Collections.Generic;
+    using System.Xml;
+    using Newtonsoft.Json;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Use: dotnet run <json_file>");
+                return;
+            }
+            string jsonFile = args[0];
+
+            List<Student> students = LoadData(jsonFile);
+
+            DisplayMainMenu();
+            int choice = AskChoice();
+
+            while (choice != 0)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        ManageStudentsMenu(students);
+                        break;
+                    case 2:
+                        ManageCoursesMenu();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+                DisplayMainMenu();
+                choice = AskChoice();
+            }
+            SaveData(jsonFile, students);
+        }
+
+        static List<Student> LoadData(string jsonFile)
+        {
+            if (!File.Exists(jsonFile))
+            {
+                Console.WriteLine($"File '{jsonFile}' does not exist.");
+                return new List<Student>();
+            }
+
+            string jsonData = File.ReadAllText(jsonFile);
+            List<Student> students = JsonConvert.DeserializeObject<List<Student>>(jsonData);
+            return students;
+        }
+
+        static void SaveData(string jsonFile, List<Student> students)
+        {
+            string jsonData = JsonConvert.SerializeObject(students, Formatting.Indented);
+            File.WriteAllText(jsonFile, jsonData);
+        }
+
+
+
+
+    }
 }
